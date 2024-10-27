033d3ead72ebbd32c33a327053059b171e2040e2
ram9 <178963+robotarmy@users.noreply.github.com>
2024-10-26 20:05:53 -0400

    too many secrets

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 podman pull quay.io/coreos/butane:release
 echo "updating farmkun.ign"
-podman run --replace --mount=type=glob,src=../.ssh/id_ed*,dst=/ssh,z,ro=true --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict --files-dir /ssh/ < core-os.yaml > farmkun.ign
+podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict --files-dir /ssh/ < core-os.yaml > farmkun.ign
 
 #docker run ---volume ~core/.ssh:/ssh -i quay.io/coreos/butane:release --pretty --strict --files-dir /ssh/ < core-os.yaml > farmkun.ign
