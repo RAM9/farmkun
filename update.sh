f4b9299075758ba80e453a18ce5270729c2864ee
ram9 <curtis@ram9.cc>
2024-11-10 22:38:05 -0500

    yaml for different kinds of hosts - some basic kubernetes

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 podman pull quay.io/coreos/butane:release
-echo "updating farmkun.ign"
-podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict --files-dir /ssh/ < core-os.yaml > farmkun.ign
+echo "updating farmkun*.ign and farmbot.ign"
+echo "farmbot"
+podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmbot.yaml > farmbot.ign
+echo "farmkun-generic-server"
+podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmkun-generic-server.yaml > farmkun.yaml
+#echo "farmkun2"
+#podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmkun2.yaml > farmkun2.ign
+#echo "farmkun3"
+#podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmkun3.yaml > farmkun3.ign
 
-#docker run ---volume ~core/.ssh:/ssh -i quay.io/coreos/butane:release --pretty --strict --files-dir /ssh/ < core-os.yaml > farmkun.ign
