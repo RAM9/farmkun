a5c9af580396ea0670eeeee18f66b4c890beb156
ram9 <178963+robotarmy@users.noreply.github.com>
2024-11-14 19:13:35 -0500

    patches for new host and changes to whatever wierd stuff happend in new fcos

--- update.sh
+++ update.sh
@@ -2,11 +2,6 @@
 podman pull quay.io/coreos/butane:release
 echo "updating farmkun*.ign and farmbot.ign"
 echo "farmbot"
-podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmbot.yaml > farmbot.ign
 echo "farmkun-generic-server"
-podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmkun-generic-server.yaml > farmkun.yaml
-#echo "farmkun2"
-#podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmkun2.yaml > farmkun2.ign
-#echo "farmkun3"
-#podman run --replace --name butane-farmkun-update -i quay.io/coreos/butane:release --pretty --strict < farmkun3.yaml > farmkun3.ign
-
+podman run --replace --name butane-farmkun-update -v ./:/local  -i quay.io/coreos/butane:release -d /local --strict --pretty < farmkun-generic-server.yaml > farmkun.ign
+podman run --replace --name butane-farmkun-update -v ./:/local  -i quay.io/coreos/butane:release -d /local --strict --pretty < farmbot.yaml > farmbot.ign
