cbe045d70f7b0669391232e660554c66440f6561
ram9 <curtis@ram9.cc>
2024-11-14 19:33:19 -0500

    default to permissive while debugging longhorn

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ podman pull quay.io/coreos/butane:release
 echo "updating farmkun*.ign and farmbot.ign"
 echo "farmbot"
 echo "farmkun-generic-server"
-podman run --replace --name butane-farmkun-update -v ./:/local  -i quay.io/coreos/butane:release -d /local --strict --pretty < farmkun-generic-server.yaml > farmkun.ign
-podman run --replace --name butane-farmkun-update -v ./:/local  -i quay.io/coreos/butane:release -d /local --strict --pretty < farmbot.yaml > farmbot.ign
+podman run --replace --name butane-farmkun-update -v ./local:/local  -i quay.io/coreos/butane:release -d /local --strict --pretty < farmkun-generic-server.yaml > farmkun.ign
+podman run --replace --name butane-farmkun-update -v ./local:/local  -i quay.io/coreos/butane:release -d /local --strict --pretty < farmbot.yaml > farmbot.ign
